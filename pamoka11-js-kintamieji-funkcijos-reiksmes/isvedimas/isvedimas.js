eca9f895d8104948b7e9733baf3c7e9cb23321cc
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2023-05-03 19:16:18 +0300

    pamoka 11 js ivadas ir kintamieji

---
+++ pamoka11-js-kintamieji-funkcijos-reiksmes/isvedimas/isvedimas.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    
+    // Čia yra išvedimai su alertu
+    // dar kažkas
+    alert(25);      // čia išveda į alert 25
+    alert(30);      // čia išveda į alert 30
+    alert("čia yra kažkoks tekstas ... dar kažką");     // čia išveda į alert įvestą tekstą tarp kabučių
+
+    // Čia yra išvedimai į konsolę
+    console.log(147);
+    console.log("ČIa yra kažkoks tekstas");
+    console.log(-200);
+
+
+}
