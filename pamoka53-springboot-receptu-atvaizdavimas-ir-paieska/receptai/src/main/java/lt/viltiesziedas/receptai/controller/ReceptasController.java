b56a746004fefd7254c29d7cf1f7d5775ca042fa
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2023-08-30 20:12:07 +0300

    pamoka 53

---
+++ pamoka53-springboot-receptu-atvaizdavimas-ir-paieska/receptai/src/main/java/lt/viltiesziedas/receptai/controller/ReceptasController.java
@@ -0,0 +1,32 @@
+package lt.viltiesziedas.receptai.controller;
+
+import lt.viltiesziedas.receptai.model.entity.Receptas;
+import lt.viltiesziedas.receptai.model.repository.ReceptasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ReceptasController {
+
+    @Autowired
+    ReceptasRepository receptasRepository;
+
+    @GetMapping("/receptai/paieska")
+    public String receptuPaieska(Model model, @RequestParam(required = false) String pavadinimas) {
+        List<Receptas> visiReceptai;
+        if (StringUtils.isEmpty(pavadinimas)) {
+            visiReceptai = receptasRepository.findAll();
+        } else {
+            visiReceptai = receptasRepository.findByPavadinimasLike("%" + pavadinimas + "%");
+        }
+        model.addAttribute("visiReceptai", visiReceptai);
+        return "receptai/paieska.html";
+    }
+}
---
+++ pamoka53-springboot-receptu-atvaizdavimas-ir-paieska/receptai/src/main/java/lt/viltiesziedas/receptai/model/repository/ReceptasRepository.java
@@ -0,0 +1,13 @@
+package lt.viltiesziedas.receptai.model.repository;
+
+import lt.viltiesziedas.receptai.model.entity.Receptas;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReceptasRepository extends JpaRepository<Receptas, Long> {
+    List<Receptas> findByPavadinimasLike(String pavadinimas);
+
+}
